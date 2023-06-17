6db9d8619711a64d4a246daedd85bcc1f8ce9be0
alfiartya23 <alfiartya.diwanda@gmail.com>
2023-06-17 16:43:30 +0700

    Initialize leaflet map and current position

--- js/script.js
+++ js/script.js
@@ -10,3 +10,31 @@ const inputDistance = document.querySelector(".form__input--distance");
 const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
+
+// Getting the user current location
+if (navigator.geolocation) {
+  // If the user allow the location prompt
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      // Getting the latitude and longitude
+      const { latitude, longitude } = position.coords;
+
+      //   Initialize the map object from Leaflet and set its view to choosen geo coordinates
+      const currentCoords = [latitude, longitude];
+      const map = L.map("map").setView(currentCoords, 16);
+      console.log(map);
+
+      //   Adding tile layer (OpenStreetMap tile layer)
+      L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      }).addTo(map);
+
+      L.marker(currentCoords).addTo(map).bindPopup("I'm here!").openPopup();
+    },
+    // if user block the location prompt
+    function (error) {
+      console.error(error.message);
+    }
+  );
+}
