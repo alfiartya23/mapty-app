95b93a34f770d9c936a0eb4fd4af1357d5075fdd
alfiartya23 <alfiartya.diwanda@gmail.com>
2023-06-21 09:58:39 +0700

    Refactor code project

--- js/script.js
+++ js/script.js
@@ -11,67 +11,98 @@ const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
 
-// Create a global variable for map and map event
-let map, mapEvent;
+// Refactoring the code with Class
+class App {
+  // Private instance properties
+  #map;
+  #mapEvent;
 
-// Getting the user current location
-if (navigator.geolocation) {
-  // If the user allow the location prompt
-  navigator.geolocation.getCurrentPosition(
-    function (position) {
-      // Getting the latitude and longitude
-      const { latitude, longitude } = position.coords;
+  constructor() {
+    // By calling this getPosition, the constructor is also executed imediately as the page loads
+    this._getPosition();
 
-      // Initialize the map object from Leaflet and set its view to choosen geo coordinates
-      const currentCoords = [latitude, longitude];
-      map = L.map("map").setView(currentCoords, 15);
+    // Attach the form submit so it will immediately run as the page loads
+    // When user press enter to submit data to the form
+    form.addEventListener("submit", this._newWorkout.bind(this));
 
-      // Adding tile layer (OpenStreetMap tile layer)
-      L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-        maxZoom: 19,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-      }).addTo(map);
+    // Handle toggle between cycling and running
+    // We don't need to bind with "this", because it still point to the select option
+    inputType.addEventListener("change", this._toggleElevationField);
+  }
 
-      // Displaying a map marker when user click the map
-      // Using the map variable then attach the event listener from the leaflet library
-      map.on("click", function (eventMap) {
-        mapEvent = eventMap;
-        form.classList.remove("hidden");
-        inputDistance.focus();
-      });
-    },
-    // if user block the location prompt
-    function (error) {
-      console.error(error.message);
+  // Getting Position
+  _getPosition() {
+    // Getting the user current location
+    if (navigator.geolocation) {
+      // If the user allow the location prompt
+      navigator.geolocation.getCurrentPosition(
+        this._loadMap.bind(this),
+        // if user block the location prompt
+        function (error) {
+          console.error(error.message);
+        }
+      );
     }
-  );
+  }
+
+  // Load the map and take position as parameter
+  _loadMap(position) {
+    // Getting the latitude and longitude
+    const { latitude, longitude } = position.coords;
+
+    // Initialize the map object from Leaflet and set its view to choosen geo coordinates
+    const currentCoords = [latitude, longitude];
+
+    // Display map
+    this.#map = L.map("map").setView(currentCoords, 15);
+
+    // Adding tile layer (OpenStreetMap tile layer)
+    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    }).addTo(this.#map);
+
+    // Displaying a map marker when user click the map
+    // Using the map variable then attach the event listener from the leaflet library
+    this.#map.on("click", this._showForm.bind(this));
+  }
+
+  // Display the workout form
+  _showForm(eventMap) {
+    // Remove the hidden class in workout form
+    this.#mapEvent = eventMap;
+    form.classList.remove("hidden");
+    inputDistance.focus();
+  }
+
+  // Toggle the elevation field
+  _toggleElevationField() {
+    // Remember! Here select the closest parent from the elevation input
+    inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
+    inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
+  }
+
+  // New workout
+  _newWorkout(e) {
+    e.preventDefault();
+
+    inputDistance.value = inputCadence.value = inputDuration.value = inputElevation.value = "";
+
+    const { lat, lng } = this.#mapEvent.latlng;
+    L.marker([lat, lng])
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          classname: "running-popup",
+        })
+      )
+      .setPopupContent("I'm here")
+      .openPopup();
+  }
 }
 
-// When user press enter to submit data to the form
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
-
-  inputDistance.value = inputCadence.value = inputDuration.value = inputElevation.value = "";
-
-  const { lat, lng } = mapEvent.latlng;
-  L.marker([lat, lng])
-    .addTo(map)
-    .bindPopup(
-      L.popup({
-        maxWidth: 250,
-        minWidth: 100,
-        autoClose: false,
-        closeOnClick: false,
-        classname: "running-popup",
-      })
-    )
-    .setPopupContent("I'm here")
-    .openPopup();
-});
-
-// Handle toggle between cycling and running
-inputType.addEventListener("change", function () {
-  // Remember! Here select the closest parent from the elevation input
-  inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
-  inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
-});
+const app = new App();
